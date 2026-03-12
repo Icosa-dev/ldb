e03a01b9c4c71920008f9a0b905b7731c98d89f7
Icosa-dev <ljcottendev@gmail.com>
2026-03-12 01:28:02 -0400

    Added test for binary trees

---
+++ test/binary_tree_test.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+int main(void)
+{
+    printf("Initial Commit\n");
+    return 0;
+}
