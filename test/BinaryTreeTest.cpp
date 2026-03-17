8c0325cdc24ad6de43cf134f6766a99b95bd849b
Icosa-dev <ljcottendev@gmail.com>
2026-03-16 23:03:04 -0400

    Added test for binary tree

---
+++ test/BinaryTreeTest.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2026 LJC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "BinaryTree.hpp"
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
